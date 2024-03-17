3c57af35e8b2bc510f9c88bfd71fa5b5a7f73294
Muhsinullah <muhsinkhanj786@gmail.com>
2024-03-18 00:28:04 +0500

    modified

--- chapter 04/js/main.js
+++ chapter 04/js/main.js
@@ -1,4 +1,37 @@
+///|| Chapter NO => 04  ||\\
+
+// //  (Question No => 01)
 var a = 5, b = "Hello", c = true;
+
+
+// //  (Question No => 02)
+/////  illegal 
+var 1stname = 'muhsin';
+var last-name = "Ullah";
+var class = "webdevelopment";
+var batch? = 11;
+var center location = "Saylani Headoffice";
+
+//// Legal
+var noOfStudent = 60;
+var studen_name = "ali";
+var IsStudentPass = true;
+var rollNo = 30;
+var studentadress;
+
+
+// //  (Question No => 03)
+ // Assign values to variables
+ var placeholders = "$, _, letters, numbers";
+ var starters = "$, _ or letters";
+ var casing = "sensitive";
+ var js_reserved = "keywords";
+
+ // Update HTML content with JavaScript variables
+ document.getElementById("placeholders").innerText = placeholders;
+ document.getElementById("starters").innerText = starters;
+ document.getElementById("casing").innerText = casing;
+ document.getElementById("js_reserved").innerText = js_reserved;
 
 
 
